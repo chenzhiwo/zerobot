2371c4dea5370865bee3df0e7fd39f221719fec3
chenzhiwo <chenzhiwo@139.com>
2019-03-10 16:06:49 +0800

    4WD robot.

--- zerobot_driver/include/zerobot_driver/diff_drive.h
+++ zerobot_driver/include/zerobot_driver/diff_drive.h
@@ -46,11 +46,14 @@ private:
     MsgWheel wheel;
     wheel.data[0] = twist->linear.x + (twist->angular.z * wheel_distance_) / 2;
     wheel.data[1] = twist->linear.x - (twist->angular.z * wheel_distance_) / 2;
+    wheel.data[2] = twist->linear.x - (twist->angular.z * wheel_distance_) / 2;
+    wheel.data[3] = twist->linear.x + (twist->angular.z * wheel_distance_) / 2;
     msgPost(MSG_TOPIC_WHEEL, &wheel, sizeof(wheel));
   }
 
   void encoderCallback(void* payload)
   {
+    // ROS_INFO_STREAM("ENCODER>>>");
     MsgEncoder* encoder = reinterpret_cast<MsgEncoder*>(payload);
     ros::Time now = ros::Time::now();
 
